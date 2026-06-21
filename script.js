c3db16a65556bd2d14b45dc93f62ae93825c63e2
Raj-0126 <rajanjankar1@gmail.com>
2026-06-21 22:03:21 +0530

    Added back particles

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/*const canvas = document.getElementById("particles");
+const canvas = document.getElementById("particles");
 const ctx = canvas.getContext("2d");
 
 canvas.width = window.innerWidth;
@@ -57,4 +57,3 @@ window.addEventListener("resize",()=>{
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
 });
-*/
